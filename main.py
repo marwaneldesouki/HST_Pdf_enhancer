1d2da6f88cad43f55f620f8b1fc9cd96ca6b853b
marwan eldesouki <moramoatazmoataz@yahoo.com>
2025-01-06 17:06:29 +0200

    update: make the correction list dynamic by using camelot to get words from cells then tokenize the words and then by using difflib.get_close_matches to replace the wrong words that was extracted by OCR
    add: multi threading to extract text from images to speed up the process

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-from functions import *
-
-pdfs_tables = get_pdf_tables([r"D:\my work\python\HST_Pdf_enhancer\documents\الاثنين 16-12-2024 د19 القاهرة الجديدة.pdf"])
-
-print(pdfs_tables)
-
-
-tablesList= get_tableList_from_tables(pdfs_tables)
-
-
-print(tablesList[0][0])
-# for i,tableList in enumerate(tablesList):
-#     tableList.to_csv(f"table_{i}.csv")
+from difflib import get_close_matches
+import arabic_reshaper
+from bidi.algorithm import get_display
+correct_lines = ['ﻣﻮﻗﻊ', 'ﺍﻟﻄﺒﺎﻋﺔ:', 'ﻧﻴﺎﺑﺔﺷﺮﻕ', 'ﺍﻟﻘﺎﻫﺮﺓ', 'ﺍﻟﻜﻠﻴﺔ', 'ﻟﺸﺌﻮﻥ', 'ﺍﻻﺳﺮﺓ', 'ﺍﻟﻘﺎﻫﺮﺓ', 'ﺍﻟﺠﺪﻳﺪﺓ', 'ﺍﻟﺠﺰﺋﻴﺔ', 'ﻟﺸﺌﻮﻥ', 'ﺍﻻﺳﺮﺓ', 'ﻧﻴﺎﺑﺔ', 'ﺃﺟﻨﺪﺓ', 'ﻳﻮﻣﻴﺔ', 'ﺟﻠﺴﺎﺕ', 'ﻧﻤﻮﺫﺝ', 'ﺭﻗﻢ', '42', 'ﺷﺌﻮﻥ', 'ﺃﺳﺮﺓ', '19', 'ﺑﺪﺍﺋﺮﺓ', '2024/12/16', 'ﻳﻮﻣﻴﺔ', 'ﺟﻠﺴﺔ', 'ﻳﻮﻡ', 'ﺍﻟﻤﻮﺍﻓﻖ', 'ﺭﺋﻴﺲ', 'ﺍﻟﻤﺤﻜﻤﺔ', 'ﻧﺰﺍﺭ', 'ﻋﺒﺪﺍﻟﻔﺘﺎﺡ', 'ﺑﺮﺋﺎﺳﺔ', 'ﺍﻟﻘﺎﺿﻲ', 'ﺍﻟﻘﺎﺿﻴﻴﻦ', 'ﻣﺤﻤﺪ', 'ﺍﻟﺸﻌﺮﺍﻭﻯ', 'ﻭ', 'ﻣﺤﻤﻮﺩ', 'ﺩﻳﺎﺏ', 'ﻭﻋﻀﻮﻳﺔ', 'ﻛﻞ', 'ﻣﻦ', 'ﻛﺮﻳﻢ', 'ﺣﺴﻦ', 'ﻋﻀﻮ', 'ﺍﻟﺪﺍﺋﺮﺓ', 'ﺍﻟﺨﺎﻣﺲ', 'ﺿﻴﺎﺀ', 'ﺍﻟﺪﻳﻦ', 'ﺍﻻﺣﻤﺪﺍﻭﻯ', 'ﻭﺑﺤﻀﻮﺭ', 'ﻭﺑﺤﻀﻮﺭ', 'ﻋﻀﻮ', 'ﺍﻟﺪﺍﺋﺮﺓ', 'ﺍﻟﺮﺍﺑﻊ', 'ﺍﺣﻤﺪ', 'ﻣﺪﺣﺖ', 'ﻧﻮﺭ', 'ﺍﻟﺪﻳﻦ', 'ﺍﺣﻤﺪ', 'ﻋﻀﻮ', 'ﺍﻟﻨﻴﺎﺑﺔ', 'ﻭﺑﺤﻀﻮﺭ', 'ﻭﺑﺤﻀﻮﺭ', 'ﺍﻟﺨﺒﻴﺮﻳﻦ', 'ﻣﻨﻰ', 'ﻓﺎﻳﺰ', 'ﺍﺑﻮﺍﻟﻮﻓﺎ', 'ﻣﺤﻤﺪ', 'ﺍﻣﻴﺮﻩ', 'ﻋﺎﻃﻒ', 'ﻋﺒﺪﺍﻟﺮﺍﺯﻕ', 'ﺧﺒﻴﺮ', 'ﻧﻔﺴﻲ', 'ﺧﺒﻴﺮ', 'ﺍﺟﺘﻤﺎﻋﻲ', 'ﻧﺎﺻﺮ', 'ﺳﺎﻣﻰ', 'ﻋﻄﻴﻪ', 'ﺧﻠﻴﻞ', 'ﺳﻜﺮﺗﻴﺮ', 'ﺍﻟﺠﻠﺴﺔ', 'ﻭﺑﺤﻀﻮﺭ', 'ﻓﺘﺤﺖ', 'ﺍﻟﺠﻠﺴﺔ', 'ﺍﻟﺴﺎﻋﻪ', 'ﻭﻗﻔﻠﺖ', 'ﺍﻟﺴﺎﻋﺔ']
+def fix_arabic_text(cell): # Fix Arabic text direction
+    try:
+        reshaped_text = arabic_reshaper.reshape(cell)  # Reshape Arabic characters
+        bidi_text = get_display(reshaped_text)  # Apply BiDi algorithm
+        return bidi_text
+    except Exception:
+        return cell  # Return as is if not Arabic
+correct_lines = [fix_arabic_text(line) for line in correct_lines]
+print(correct_lines)
+print(get_close_matches(fix_arabic_text('الساغة'), correct_lines, n=1, cutoff=0.7))
